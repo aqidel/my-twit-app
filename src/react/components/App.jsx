194ba65918121c01017cbe8bc6b379f3e3f1e741
Arsen-PC <fanofparadox@gmail.com>
2022-01-01 16:53:25 +0500

    windows test

--- src/react/components/App.jsx
+++ src/react/components/App.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Sidebar from './Sidebar.jsx';
+import '../../styles/main.scss';
 
 export default function App() {
   return (
-    <div>Hello, world!</div>
+    <>
+      <Sidebar/>
+    </>
   )
 }
---
+++ src/react/components/Sidebar.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Sidebar() {
+    return <p>Hello, world!</p>;
+}
