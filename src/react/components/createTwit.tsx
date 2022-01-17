569af8d8f6e52e292fdb080112d2fe703b414803
Edward <fanofparadox@gmail.com>
2022-01-17 09:07:02 +0300

    svg component added

--- src/react/components/createTwit.tsx
+++ src/react/components/createTwit.tsx
@@ -2,6 +2,7 @@ import React, { FormEvent } from 'react';
 import { useDispatch } from 'react-redux';
 import { getFirestore, collection, doc, setDoc } from 'firebase/firestore';
 import { toggle } from '../mainSlice';
+import { CloseBtn } from '../svg/SVGs';
 
 export default function CreateTwit() {
   
@@ -22,16 +23,7 @@ export default function CreateTwit() {
     <div id='modal-wrap'>
       <div id='modal'>
         <div id='close-modal' onClick={() => dispatch(toggle())}>
-          <svg viewBox='0 0 24 24' aria-hidden='true'>
-            <g>
-              <path d='M13.414 12l5.793-5.793c.39-.39.39-1.023 0-1.414s-1.023-.39-1.414 
-                       0L12 10.586 6.207 4.793c-.39-.39-1.023-.39-1.414 0s-.39 1.023 0 
-                       1.414L10.586 12l-5.793 5.793c-.39.39-.39 1.023 0 
-                       1.414.195.195.45.293.707.293s.512-.098.707-.293L12 13.414l5.793 
-                       5.793c.195.195.45.293.707.293s.512-.098.707-.293c.39-.39.39-1.023 0-1.414L13.414 12z'>
-              </path>
-            </g>
-          </svg>
+          <CloseBtn/>
         </div>
         <div id='create-twit-wrap'>
           <div id='create-twit-avatar'></div>
